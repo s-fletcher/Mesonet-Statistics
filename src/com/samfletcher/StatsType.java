a4b57c4c6e38995eeca2551ef9700ff082069c4e
Sam Fletcher <samfletcher@ou.edu>
2018-11-26 13:58:37 -0600

    Initial commit (copied from project3)

---
+++ src/com/samfletcher/StatsType.java
@@ -0,0 +1,16 @@
+package com.samfletcher;
+
+/**
+ * @author Sam Fletcher
+ * @version 10-26-18
+ * Project 3
+ * 
+ * Enumerator to separate statistics into four categories.
+ */
+public enum StatsType
+{
+    AVERAGE,
+    MINIMUM,
+    MAXIMUM,
+    TOTAL
+}
---
+++ src/com/samfletcher/Tests/AllTests.java
@@ -0,0 +1,12 @@
+package com.samfletcher.Tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ MapDataTest.class, ObservationTest.class, StatisticsTest.class })
+public class AllTests
+{
+
+}
